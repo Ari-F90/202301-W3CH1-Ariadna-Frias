4733d4fcac414fd0b682e129a3ced523661d1e40
Ariadna Frias <ariadna.812@gmail.com>
2023-01-31 18:46:25 +0100

    Moved class files and tests in folder models

---
+++ src/models/characters.ts
@@ -0,0 +1,10 @@
+import { Character } from './character';
+import { King } from './king';
+import { Fighter } from './fighter';
+import { Advisor } from './advisor';
+import { Squire } from './squire';
+
+const kingJoffrey = new King('Joffrey', 'Baratheon');
+const;
+
+const characters = [];
